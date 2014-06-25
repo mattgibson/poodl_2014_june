7c804a894a7aac0dc955f4b2fa590dbfd8cd5a6b
Matt Gibson <downrightlies@gmail.com>
2014-06-25 16:07:38 +0100

    Mostly DRY

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -6,13 +6,9 @@ class Bottles
   def verse(number)
     case number
     when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+      "#{number_representation(number).to_s.capitalize} #{word_for_bottles(number)} of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, #{number_representation(next_number_of_bottles(number))} #{word_for_bottles(number)} of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
+      "#{number_representation(number).to_s.capitalize} #{word_for_bottles(number)} of beer on the wall, #{number} #{word_for_bottles(number)} of beer.\nTake #{word_for_what_to_take_down(number)} down and pass it around, #{number_representation(next_number_of_bottles(number))} #{word_for_bottles(next_number_of_bottles(number))} of beer on the wall.\n"
     end
   end
 
@@ -23,4 +19,25 @@ class Bottles
   def song
     verses(99, 0)
   end
+
+  def word_for_bottles(number)
+    number == 1 ? 'bottle' : 'bottles'
+  end
+
+  def word_for_what_to_take_down(number)
+    number == 1 ? 'it' : 'one'
+  end
+
+  def number_representation(number)
+    number == 0 ? 'no more' : number
+  end
+
+  def next_number_of_bottles(number)
+    number == 0 ? 99 : number -1
+  end
+
+  def thing_to_do_next(number)
+
+  end
+
 end
