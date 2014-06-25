4502c3773a1e0109a8c3909382b9592c5d04b448
Matt Gibson <downrightlies@gmail.com>
2014-06-25 16:34:38 +0100

    Extra method extractions

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,12 +1,11 @@
 # Feel free to delete the instructions once you get going
-puts instructions(__FILE__)
 
 class Bottles
 
   def verse(number)
-    "#{number_representation(number).to_s.capitalize} #{word_for_bottles(number)} of beer on the wall, "+
-        "#{number_representation(number)} #{word_for_bottles(number)} of beer.\n#{thing_to_do_next(number)}, "+
-        "#{number_representation(next_number_of_bottles(number))} #{word_for_bottles(next_number_of_bottles(number))} of beer on the wall.\n"
+    "#{number_of_bottles(number)} of beer on the wall, ".capitalize +
+        "#{number_of_bottles(number)} of beer.\n#{thing_to_do_next(number)}, "+
+        "#{number_of_bottles(next_number_of_bottles(number))} of beer on the wall.\n"
   end
 
   def verses(end_verse, start_verse)
@@ -14,27 +13,45 @@ class Bottles
   end
 
   def song
-    verses(99, 0)
+    verses(number_of_bottles_at_the_start, 0)
   end
 
+
   def word_for_bottles(number)
-    number == 1 ? 'bottle' : 'bottles'
+    one_bottle_left?(number) ? 'bottle' : 'bottles'
   end
 
   def word_for_what_to_take_down(number)
-    number == 1 ? 'it' : 'one'
+    one_bottle_left?(number) ? 'it' : 'one'
   end
 
   def number_representation(number)
-    number == 0 ? 'no more' : number
+    no_bottles_left?(number) ? 'no more' : number
   end
 
   def next_number_of_bottles(number)
-    number == 0 ? 99 : number -1
+    no_bottles_left?(number) ? number_of_bottles_at_the_start : number -1
   end
 
   def thing_to_do_next(number)
-    number == 0 ? "Go to the store and buy some more" : "Take #{word_for_what_to_take_down(number)} down and pass it around"
+    no_bottles_left?(number) ? "Go to the store and buy some more" : "Take #{word_for_what_to_take_down(number)} down and pass it around"
+  end
+
+  def number_of_bottles(number)
+    "#{number_representation(number)} #{word_for_bottles(number)}"
+  end
+
+
+  def no_bottles_left?(number)
+    number == 0
+  end
+
+  def one_bottle_left?(number)
+    number == 1
+  end
+
+  def number_of_bottles_at_the_start
+    99
   end
 
 end
