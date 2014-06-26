2cf7f124f0082bd3e101aa42f99a4ec6ad76a971
Matt Gibson <downrightlies@gmail.com>
2014-06-26 16:24:00 +0100

    Works and clean

--- farm/lib/farm.rb
+++ farm/lib/farm.rb
@@ -1,6 +1,22 @@
-# Feel free to delete the instructions once you get going
-puts instructions(__FILE__)
+using Article
 
 class Farm
 
+  attr_accessor :animals
+
+  def initialize(animals)
+    @animals = animals.to_a
+  end
+
+  def lyrics
+    ::Animal.all(animals).collect { |instance| verse(instance) }.join("\n\n")
+  end
+
+  def verse(animal = nil)
+    %Q[Old MacDonald had a farm, E-I-E-I-O,
+And on that farm he had #{animal.species.articlize}, E-I-E-I-O,
+With #{animal.sound.articlize} #{animal.sound} here and #{animal.sound.articlize} #{animal.sound} there,
+Here #{animal.sound.articlize}, there #{animal.sound.articlize}, everywhere #{animal.sound.articlize} #{animal.sound},
+Old MacDonald had a farm, E-I-E-I-O.]
+  end
 end
