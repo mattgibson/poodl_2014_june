b80981806af9f5473bf45e249d08babb90c9365e
Matt Gibson <downrightlies@gmail.com>
2014-06-26 16:44:02 +0100

    With nil test fixed

--- farm/lib/farm.rb
+++ farm/lib/farm.rb
@@ -5,18 +5,43 @@ class Farm
   attr_accessor :animals
 
   def initialize(animals)
-    @animals = animals.to_a
+    @animals = animals
   end
 
   def lyrics
-    ::Animal.all(animals).collect { |instance| verse(instance) }.join("\n\n")
+    get_animals.collect { |instance| verse(instance) }.join("\n\n")
+  end
+
+  def get_animals
+    ::Animal.all(animals)
   end
 
   def verse(animal = nil)
     %Q[Old MacDonald had a farm, E-I-E-I-O,
-And on that farm he had #{animal.species.articlize}, E-I-E-I-O,
-With #{animal.sound.articlize} #{animal.sound} here and #{animal.sound.articlize} #{animal.sound} there,
-Here #{animal.sound.articlize}, there #{animal.sound.articlize}, everywhere #{animal.sound.articlize} #{animal.sound},
+And on that farm he had #{articlized_species(animal)}, E-I-E-I-O,
+With #{articlized_sound(animal)} #{sound(animal)} here and #{articlized_sound(animal)} #{sound(animal)} there,
+Here #{articlized_sound(animal)}, there #{articlized_sound(animal)}, everywhere #{articlized_sound(animal)} #{sound(animal)},
 Old MacDonald had a farm, E-I-E-I-O.]
   end
+
+  def articlized_species(animal)
+    if animal == nil
+      "a <silence>"
+    else
+      animal.species.articlize
+    end
+  end
+
+  def articlized_sound(animal)
+    sound(animal).articlize
+  end
+
+  def sound(animal)
+    if animal == nil
+      "<silence>"
+    else
+      animal.sound
+    end
+  end
 end
+
